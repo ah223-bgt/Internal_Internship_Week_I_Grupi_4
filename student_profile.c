d40344c5fd3643b9b0bf1210d6694d76e4bd6214
ArtQollaku <ac222@bgt.school>
2026-04-14 11:44:03 +0200

    Create student_profile.c

---
+++ student_profile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Student Profile\n");
+    printf("---------------------------\n");
+    printf("Name: Art\n");
+    printf("Surname: Collaku\n");
+    printf("School: British Gymnasium of Technology\n");
+    printf("Class: XI\n");
+
+    return 0;
+}
