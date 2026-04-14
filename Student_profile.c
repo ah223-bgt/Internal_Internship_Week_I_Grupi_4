cda167a875c226baeb2557fed82f612f853be202
Deoniii <do234@bgt.school>
2026-04-14 11:25:04 +0200

    Create Student_profile.c

---
+++ Student_profile.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char name[] = "Deon";
+    char surname[] = "Osmani";
+    char school[] = "British Gymnasium of Technology";
+    char studentClass[] = "11th Grade";
+
+    printf("---- Student Profile ----\n");
+    printf("Name     : %s\n", name);
+    printf("Surname  : %s\n", surname);
+    printf("School   : %s\n", school);
+    printf("Class    : %s\n", studentClass);
+
+    return 0;
+}
