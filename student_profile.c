78b2aee1c784781a674745156bba2a2ea1170ac0
ddooraa <da240@bgt.school>
2026-04-14 11:20:37 +0200

    Add student profile display in student_profile.c

---
+++ student_profile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Student Profile:\n");
+    printf("Emri: Filan\n");          // Shkruaj emrin tënd këtu
+    printf("Mbiemri: Fisteku\n");    // Shkruaj mbiemrin tënd këtu
+    printf("Grupi: X\n");            // Zëvendëso X me numrin e grupit tënd [cite: 11]
+    printf("Dita: 1\n");
+    
+    return 0;
+}
