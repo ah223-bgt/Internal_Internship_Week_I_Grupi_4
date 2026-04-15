93c2fb57f0b4e4c7ccab5850e854aea096450581
ah223-bgt <ah223@bgt.school>
2026-04-15 08:45:21 +0200

    Add student profile display in student_profile.c

--- Day1/student_profile.c
+++ Day1/student_profile.c
@@ -1,1 +1,18 @@
-<content of student_profile.c>
+#include <stdio.h>
+
+int main() {
+    // Student information
+    char name[] = "Art Halili";
+    int age = 17;
+    char grade[] = "11th Grade";
+    char school[] = "British Gymnasium of Technology";
+
+    // Display profile
+    printf("----- Student Profile -----\n");
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("Grade: %s\n", grade);
+    printf("School: %s\n", school);
+
+    return 0;
+}
