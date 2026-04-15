0cd9e3c7345a60b62dd8fd351439e25c199ad6f0
ArtQollaku <ac222@bgt.school>
2026-04-15 11:41:55 +0200

    Update day2_task2.c

--- Day 2/day2_task2.c
+++ Day 2/day2_task2.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main() {
+    int score;
+
+    // Marrja e input
+    printf("Shkruaj rezultatin (0 - 100): ");
+    scanf("%d", &score);
+
+    // Kontrolli i intervalit (shume e rendesishme)
+    if (score < 0 || score > 100) {
+        printf("Input jo valid! Ju lutem shkruani nje vlere nga 0 deri ne 100.\n");
+    }
+    else if (score >= 90) {
+        printf("Vleresimi: Shkelqyeshem\n");
+    }
+    else if (score >= 75) {
+        printf("Vleresimi: Shume mire\n");
+    }
+    else if (score >= 50) {
+        printf("Vleresimi: Mire\n");
+    }
+    else {
+        printf("Vleresimi: Ne permiresim\n");
+    }
+
+    return 0;
+}
