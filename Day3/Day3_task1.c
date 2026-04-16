c9f6b03aaf9baaa6451ca85c033d497943ee0d1f
ah223-bgt <ah223@bgt.school>
2026-04-16 09:52:25 +0200

    Implement pattern selection with stars or numbers

--- Day3/Day3_task1.c
+++ Day3/Day3_task1.c
@@ -1,1 +1,57 @@
+#include <stdio.h> // input/output
 
+int main()
+{
+    int n, zgjedhja;
+
+    // marrja e madhesise
+    printf("Shkruaj nje numer (1 - 12): ");
+    scanf("%d", &n);
+
+    // kontrolli i input-it
+    if (n < 1 || n > 12)
+    {
+        printf("Input i pavlefshem! Jep numer nga 1 deri ne 12.");
+        return 0; // ndalon programin
+    }
+
+    // zgjedhja e pattern-it
+    printf("Zgjidh pattern:\n");
+    printf("1 - Yje (*)\n");
+    printf("2 - Numra\n");
+    printf("Zgjedhja: ");
+    scanf("%d", &zgjedhja);
+
+    printf("\n--- PATTERN ---\n");
+
+    // pattern me yje
+    if (zgjedhja == 1)
+    {
+        for (int i = 1; i <= n; i++) // rreshtat
+        {
+            for (int j = 1; j <= i; j++) // elementet ne rresht
+            {
+                printf("* ");
+            }
+            printf("\n"); // rresht i ri
+        }
+    }
+    // pattern me numra
+    else if (zgjedhja == 2)
+    {
+        for (int i = 1; i <= n; i++) // rreshtat
+        {
+            for (int j = 1; j <= i; j++) // elementet
+            {
+                printf("%d ", j);
+            }
+            printf("\n");
+        }
+    }
+    else
+    {
+        printf("Zgjedhje e pavlefshme!");
+    }
+
+    return 0; // fundi
+}
