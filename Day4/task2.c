058a46dc81bec33ed2802ec86ac48d1ff4afe190
ddooraa <da240@bgt.school>
2026-04-17 10:28:34 +0200

    Create task2.c

---
+++ Day4/task2.c
@@ -0,0 +1,29 @@
+Kërkesa 2 - Mini-Projekt
+
+Fokusi
+
+enum, validim më i fortë i input-it dhe strukturim më i qartë i rrjedhës së menusë.
+
+Kërkesa e re
+
+Tani projekti duhet të ketë logjikë më të fortë në menunë e tij dhe një mënyrë më të qartë për të përfaqësuar statusin ose kategorinë e çdo regjistrimi.
+
+Çfarë duhet të shtoni tani
+
+• Krijoni të paktën një enum për statusin ose kategorinë e regjistrimeve.
+
+• Lidheni vlerën e enum-it me të dhënat që ruhen për secilin regjistrim.
+
+• Përdorni switch për menunë kryesore dhe edhe për të paktën një degëzim tjetër, si zgjedhja e statusit ose një nënveprim të programit.
+
+• Validoni zgjedhjet e pavlefshme të menusë dhe mos lejoni që programi të vazhdojë pa kontroll.
+
+• Validoni edhe input-et e pavlefshme për statusin ose kategorinë dhe trajtojini me mesazh të qartë.
+
+• Mbani të kuptueshme lidhjen midis asaj që përdoruesi zgjedh dhe asaj që ruhet në logjikën e programit.
+
+Kujdes gjatë zbatimit
+
+• Enum-i duhet të përdoret realisht në program dhe jo vetëm të deklarohet pa rol praktik.
+
+• Zgjedhjet e pavlefshme nuk duhet të prishin rrjedhën e menusë ose të fusin të dhëna jo të sakta në program.
