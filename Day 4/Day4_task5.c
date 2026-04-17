bff3046634e75a6ffc2957f948078a244c6c7efd
ArtQollaku <ac222@bgt.school>
2026-04-17 14:27:43 +0200

    Create Day4_task5.c

---
+++ Day 4/Day4_task5.c
@@ -0,0 +1,23 @@
+void updateRegjistrim(struct Regjistrim *r) {
+
+    printf("\n--- PERDITESIM REGJISTRIMI ---\n");
+
+    printf("Piket aktuale: %.2lf\n", r->pike);
+    printf("Piket e reja: ");
+    scanf("%lf", &r->pike);
+
+    int s;
+    printf("Statusi i ri:\n");
+    printf("1. Nevojitet ushtrim\n");
+    printf("2. Gati per vazhdim\n");
+    printf("3. Gati per sfide\n");
+    printf("Zgjedhja: ");
+    scanf("%d", &s);
+
+    if(s >=1 && s <=3)
+        r->status = s;
+    else
+        printf("Status i pavlefshem, u mbajt i vjetri.\n");
+
+    printf("Regjistrimi u perditesua me sukses!\n");
+}
